ead791fcce7529c4315b9b391b9c5cb69057dd6a
Mark Stehr <Mark.Stehr@hdi.de>
2020-03-16 09:25:01 +0100

    Spring Boot für Websphere

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # HDI Mitarbeiter Portal
 
+Spring Boot unter WebSphere 9 installieren
+	https://docs.spring.io/spring-boot/docs/current/reference/htmlsingle/#howto-create-a-deployable-war-file
+	
+* http://localhost:9080/
+* https://localhost:9443/
--- src/main/java/de/hdi/portal/Application.java
+++ src/main/java/de/hdi/portal/Application.java
@@ -2,6 +2,7 @@ package de.hdi.portal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 /**
@@ -10,8 +11,13 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 @SpringBootApplication
 public class Application extends SpringBootServletInitializer {
 
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
-    }
+	public static void main(final String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(final SpringApplicationBuilder application) {
+		return application.sources(Application.class);
+	}
 
 }
